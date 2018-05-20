ab5819672d33986edab5425d096ef21a6e577eae
Jason Trigg <jasontrigg0@gmail.com>
2018-05-19 20:07:05 -0400

    Add code

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+    name = 'python-service',
+    packages = ['pyservice'],
+    version = "0.0.0",
+    description = 'Placeholder description',
+    author = "Jason Trigg",
+    author_email = "jasontrigg0@gmail.com",
+    url = "https://github.com/jasontrigg0/python-service",
+    download_url = 'https://github.com/jasontrigg0/python-service/tarball/0.0.0',
+    scripts=[],
+    install_requires=[
+        "python-daemon",
+        "setproctitle"
+    ],
+    keywords = [],
+    classifiers = [],
+)
