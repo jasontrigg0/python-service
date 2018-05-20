2f7d2aaac52eada73a541fb4ad5085faa34dd919
Jason Trigg <jasontrigg0@gmail.com>
2018-05-19 20:18:47 -0400

    pypi version 0.0.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
     name = 'python-service',
     packages = ['pyservice'],
-    version = "0.0.2",
+    version = "0.0.3",
     description = 'Placeholder description',
     author = "Jason Trigg",
     author_email = "jasontrigg0@gmail.com",
     url = "https://github.com/jasontrigg0/python-service",
-    download_url = 'https://github.com/jasontrigg0/python-service/tarball/0.0.2',
+    download_url = 'https://github.com/jasontrigg0/python-service/tarball/0.0.3',
     scripts=[],
     install_requires=[
         "python-daemon",
